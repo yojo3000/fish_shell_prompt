9d166bdf6fdb62e11adbfc440d7b0d997924940f
Mars Huang <yojo4000@gmail.com>
2018-03-26 16:21:13 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,3 @@ cp ~/fish_shell_prompt/monitor.sh ~/.monitor.sh
 git config --global alias.logs "log --graph --abbrev-commit --decorate --date=relative --all"
 cd ~
 rm -rf ~/fish_shell_prompt
-source ~/.config/fish/config.fish
