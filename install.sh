413442a9cafebba8a7dfb653ae30d2fefe4c90ab
Mars Huang <yojo4000@gmail.com>
2018-03-26 14:56:53 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/yojo3000/fish_shell_prompt.git ~/fish_shell_prompt
 mkdir -p ~/.config/fish
 cp ~/fish_shell_prompt/config.fish ~/.config/fish/
 rm -rf ~/fish_shell_prompt
