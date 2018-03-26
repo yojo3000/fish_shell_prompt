ee50c188e1f5ac9a5c679c324edce3946dbd9fa3
Mars Huang <yojo4000@gmail.com>
2018-03-26 15:37:30 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 mkdir -p ~/.config/fish
 cp ~/fish_shell_prompt/config.fish ~/.config/fish/
+cp ~/fish_shell_prompt/monitor.sh ~/.monitor.sh
 cd ~
 rm -rf ~/fish_shell_prompt
 . ~/.config/fish/config.fish
