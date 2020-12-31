d1197298d6523ba713a70072ea44d850fa7df0df
yojo3000 <yojo4000@gmail.com>
2020-12-31 17:01:23 +0800

    add dtag command

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@ mkdir -p ~/.config/fish
 cp ~/fish_shell_prompt/config.fish ~/.config/fish/
 cp ~/fish_shell_prompt/monitor.sh ~/.monitor.sh
 git config --global alias.logs "log --graph --abbrev-commit --decorate --date=relative --all"
+sudo chmod +x dtag
+sudo cp dtag /usr/local/bin
 cd ~
 rm -rf ~/fish_shell_prompt
+
+
