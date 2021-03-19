431746aa36da061ca49da21ce9b1acc486034f78
yojo3000 <yojo4000@gmail.com>
2021-03-19 14:52:44 +0800

    fix dtag path

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ mkdir -p ~/.config/fish
 cp ~/fish_shell_prompt/config.fish ~/.config/fish/
 cp ~/fish_shell_prompt/monitor.sh ~/.monitor.sh
 git config --global alias.logs "log --graph --abbrev-commit --decorate --date=relative --all"
-sudo chmod +x dtag
-sudo cp dtag /usr/local/bin
+sudo chmod +x ~/fish_shell_prompt/dtag
+sudo cp ~/fish_shell_prompt/dtag /usr/local/bin
 cd ~
 rm -rf ~/fish_shell_prompt
 
