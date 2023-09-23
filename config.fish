4c9a7e771b4c1d7a0772b11364b1a8c1c6237474
yojo3000 <yojo4000@gmail.com>
2023-09-23 14:55:17 +0800

    fix dtag command alias

--- config.fish
+++ config.fish
@@ -12,6 +12,8 @@ alias dpaother=docker_ps_format_others
 alias drmnone="docker rmi (docker images --filter "dangling=true" -q --no-trunc)"
 alias drmexit="docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs docker rm"
 
+alias dtag=/usr/local/bin/dtag.py
+
 alias infosys="cat ~/.monitor.sh | bash"
 
 alias iplocation=ip_location
