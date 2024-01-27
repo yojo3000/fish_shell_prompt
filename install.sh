1451246d1c6da1a0ba75f8dae55c2bbd0a42e810
yojo3000 <yojo4000@gmail.com>
2024-01-27 14:40:09 +0800

    add fish functions folder

--- install.sh
+++ install.sh
@@ -12,6 +12,11 @@ if test -f "$FILE"; then
 	sudo rm /usr/local/bin/dtag
 fi
 
+DIR=~/fish_shell_prompt/functions
+if ! test -d "$DIR"; then
+	mkdir ~/.config/fish/functions
+fi
 cp ~/fish_shell_prompt/functions/* ~/.config/fish/functions/
+
 
 #rm -rf ~/fish_shell_prompt
